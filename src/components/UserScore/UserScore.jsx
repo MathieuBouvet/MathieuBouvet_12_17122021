522c0334cbc07e3622c1b218e52ad193d013fe6e
Mathieu Bouvet <mathieubouvet42@gmail.com>
2022-01-13 13:52:11 +0100

    Use recahart for the score displayer

--- src/components/UserScore/UserScore.jsx
+++ src/components/UserScore/UserScore.jsx
@@ -1,25 +1,39 @@
 import PropTypes from "prop-types";
+import { PieChart, ResponsiveContainer, Pie, Label, LabelList } from "recharts";
+
 import styles from "./userScore.module.scss";
 
 const UserScore = ({ children }) => {
+  const data = [
+    {
+      score: children,
+      fill: "#ff0000",
+    },
+    {
+      score: 100 - children,
+      fill: "transparent",
+    },
+  ];
   return (
     <div className={styles.userScore}>
-      Score
-      <div className={styles.circleContainer}>
-        <svg className={styles.svgCircle}>
-          <circle
-            cx="90"
-            cy="90"
-            r="80"
-            pathLength="100"
-            strokeDashoffset={-100 + children}
-          />
-        </svg>
-        <div className={styles.scoreDisplay}>
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart>
+          <Pie
+            data={data}
+            dataKey="score"
+            innerRadius={90}
+            outerRadius={100}
+            startAngle={90}
+            endAngle={360+90}
+            cornerRadius={5}
+          >
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+      <div className={styles.scoreDisplay}>
           <p className={styles.score}>{children}%</p>
           <p className={styles.text}>de votre objectif</p>
         </div>
-      </div>
     </div>
   );
 };
